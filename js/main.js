1523bbf0696e235aa5fbffcf2a1e55edf8aa12a9
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-10-31 10:11:00 -0400

    my list

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // Functioning to do list
 
       // Create a "close" button and append it to each list item
-      var myNodelist = document.getElementsByTagName("LI");
+      var myList = document.getElementsByTagName("LI");
       var i;
-      for (i = 0; i < myNodelist.length; i++) {
+      for (i = 0; i < myList.length; i++) {
         var span = document.createElement("SPAN");
         var txt = document.createTextNode("\u00D7");
         span.className = "close";
         span.appendChild(txt);
-        myNodelist[i].appendChild(span);
+        myList[i].appendChild(span);
       }
 
       // Click on a close button to hide the current list item
